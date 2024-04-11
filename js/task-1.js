07177dab399fcf3842d68efe73ceb3cda9c41272
MykolaSanych <ivanushkatt@gmail.com>
2024-04-11 21:29:32 +0300

    Update task-1.js

--- js/task-1.js
+++ js/task-1.js
@@ -1,2 +1,5 @@
-const theNumberOfElements = document.querySelectorAll(".item");
-console.log(theNumberOfElements);
+const items = document.querySelectorAll(".item");
+console.log(items.length);
+items.forEach(item => {
+    console.log(`${item.querySelector("h2").textContent}  ${item.querySelectorAll("ul>li").length}`);
+})
