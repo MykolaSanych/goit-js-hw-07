b0304cc60407928f388108fc7e52a80ff7fe4147
MykolaSanych <ivanushkatt@gmail.com>
2024-04-13 23:06:07 +0300

    Update task-4.js

--- js/task-4.js
+++ js/task-4.js
@@ -1,1 +1,16 @@
-
+const loginForm = document.querySelector(".login-form");
+const userEmail = loginForm.elements[0];
+const userPassword = loginForm.elements[1];
+const actionslogin = (event) => {
+  event.preventDefault();
+  if (userEmail.value && userPassword.value) {
+    const dataForm = {};
+    dataForm.Email = userEmail.value.trim();
+    dataForm.Password = userPassword.value.trim();
+    console.log(dataForm);
+    loginForm.reset();
+  } else {
+    alert("All form fields must be filled in");
+  }
+};
+loginForm.addEventListener("submit", actionslogin);
