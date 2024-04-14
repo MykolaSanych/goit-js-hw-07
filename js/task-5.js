d7774702d4f119941fa9d4301a9c40a74ed9118b
MykolaSanych <ivanushkatt@gmail.com>
2024-04-14 22:10:05 +0300

    task_5

--- js/task-5.js
+++ js/task-5.js
@@ -3,3 +3,14 @@ function getRandomHexColor() {
     .toString(16)
     .padStart(6, 0)}`;
 }
+const button = document.querySelector(".change-color");
+const body = document.querySelector("body");
+const colorName = document.querySelector(".color");
+let randomColor;
+
+const colorChange = () => {
+  randomColor = getRandomHexColor();
+  body.style.backgroundColor = randomColor;
+  colorName.textContent = randomColor;
+};
+button.addEventListener("click", colorChange);
