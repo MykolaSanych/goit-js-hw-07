af268969e56a5094c7150829e9f4cc88465645f9
MykolaSanych <ivanushkatt@gmail.com>
2024-04-15 11:51:03 +0300

    task_7

--- js/task-6.js
+++ js/task-6.js
@@ -3,3 +3,43 @@ function getRandomHexColor() {
     .toString(16)
     .padStart(6, 0)}`;
 }
+const controlElements = document.getElementById("controls");
+const controlInput = controlElements.querySelector("input");
+const controlButtons = controlElements.querySelectorAll("button");
+const paternalElement = document.getElementById("boxes");
+
+let inputNumber;
+function createBoxes(amount) {
+  let n = 0;
+  for (let i = 0; i < amount; i++) {
+    const newDiv = document.createElement("div");
+    newDiv.style.width = `${30 + n}px`;
+    newDiv.style.height = `${30 + n}px`;
+    newDiv.style.backgroundColor = getRandomHexColor();
+    paternalElement.append(newDiv);
+    n += 10;
+  }
+}
+
+controlInput.addEventListener(
+  "input",
+  (number) => (inputNumber = number.currentTarget.value)
+);
+function actionButtonCreate() {
+  if (!isNaN(inputNumber) && inputNumber >= 1 && inputNumber <= 100) {
+    removeDiv();
+    createBoxes(inputNumber);
+    controlInput.value = "";
+    inputNumber = 0;
+  } else {
+    alert("Please enter a valid number between 1 and 100.");
+  }
+}
+function removeDiv() {
+  const allDiv = paternalElement.querySelectorAll("div");
+  allDiv.forEach((div) => {
+    div.remove();
+  });
+}
+controlButtons[0].addEventListener("click", actionButtonCreate);
+controlButtons[1].addEventListener("click", removeDiv);
